772f39e433cbacc80d6e0311f63cec6ebeaaf8c2
Thijs van Loef <thijsvanloef@yoast.com>
2021-03-03 10:56:15 +0100

    Added Bank Transaction Schema & Mapping

--- tap_twinfield/streams.py
+++ tap_twinfield/streams.py
@@ -13,6 +13,45 @@ STREAMS: MappingProxyType = MappingProxyType({
         'mapping': {
             'id': {
                 'map': 'id', 'null': False,
+            },
+            'Periode': {
+                'map': 'yearperiod', 'type': date_parser, 'null': False,
+            },
+            'Bank': {
+                'map': 'bank_code', 'null': False,
+            },
+            'Bank Naam': {
+                'map': 'bank_shortname', 'null': False,
+            },
+            'Boekst.nr.': {
+                'map': 'entry_number', 'type': int, 'null': False,
+            },
+            'Afschriftnr.': {
+                'map': 'statement_number', 'type': int, 'null': False,
+            },
+            'Grootboek': {
+                'map': 'ledger', 'null': False,
+            },
+            'Grootboek Naam': {
+                'map': 'ledger_name', 'null': False,
+            },
+            'Valuta': {
+                'map': 'curcode', 'null': False,
+            },
+            'Bedrag': {
+                'map': 'value_signed', 'type': Decimal, 'null': False,
+            },
+            'Euro': {
+                'map': 'basevalue_signed', 'type': Decimal, 'null': False,
+            },
+            'Vorig saldo': {
+                'map': 'startvalue', 'type': Decimal, 'null': False,
+            },
+            'Eindsaldo': {
+                'map': 'closevalue', 'type': Decimal, 'null': False,
+            },
+            'Status': {
+                'map': 'status', 'null': False,
             }
         },
     },
